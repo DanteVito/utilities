5266178bc5070094317e095b017758db499b1d13
DanteVito <dantevitobeagland@gmail.com>
2017-04-03 16:33:47 -0300

    Add files via upload

--- re_for_numbers.py
+++ re_for_numbers.py
@@ -1,10 +1,6 @@
 # importing packages
 import re
 
-vltotal = ["vl. 1,00", "vl. total. 21,02", "vl:. R$3999,00"]
-qtd = ["qtd.: 2", "qtd.: 300", "qtd.:987"]
-
-
 pattern = "[1-9]*,[0-9][0-9]|[0-9][0-9]*"
 
 def ret_values(pattern, lista, type):
@@ -15,5 +11,3 @@ def ret_values(pattern, lista, type):
 		f_str = type(f_str.replace(',','.'))
 		result.append(f_str)
 	return result
-
-print(ret_values(pattern,qtd,int))
